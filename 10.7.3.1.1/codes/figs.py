75215eb54ecc087aad12df0cdffab2ad3d24557a
SurajBhyri <surajbhyri11@gmail.com>
2023-08-10 18:26:32 +0530

    Uploaded files

--- 10.7.3.1.1/codes/figs.py
+++ 10.7.3.1.1/codes/figs.py
@@ -34,3 +34,26 @@ def line_gen(A,B):
 x_AB = line_gen(A,B)
 x_BC= line_gen(B,C)
 x_CA = line_gen(C,A)
+
+plt.plot(x_AB[0,:],x_AB[1,:])
+plt.plot(x_BC[0,:],x_BC[1,:])
+plt.plot(x_CA[0,:],x_CA[1,:])
+
+
+#Labelling the coordinates
+tri_coords = np.vstack((A,B,C)).T
+plt.scatter(tri_coords[0,:], tri_coords[1,:])
+vert_labels= ['A','B','C']
+for i, txt in enumerate(vert_labels):
+    plt.annotate(txt, #this is text
+    (tri_coords[0,i], tri_coords[1,i]), #this is the point to label
+    textcoords="offset points" , # How to position the text
+    xytext=(0,10),#Distance from the text to points (x,y)
+    ha='center') # horizontal alignment can be left , right or center
+plt.xlabel("X")
+plt.ylabel("Y")
+#plt.legend(loc='best')
+plt.grid()
+plt.axis('equal')
+
+plt.show()
